be5a8049122a8f7c17cdcf06860aff6de663f300
Lukas Ruebbelke <simpul@gmail.com>
2013-10-24 06:33:38 -0700

    Adding start and finish states

--- js/app.finish.js
+++ js/app.finish.js
@@ -8,17 +8,23 @@ angular.module('website', ['ngAnimate'])
 
         $scope.currentPage = 'home';
         $scope.page = $scope.pages['home'];
+        $scope.isInTransit = false;
 
         $scope.setCurrentPage = function (page) {
             if ($scope.currentPage !== page) {
                 $scope.page = $scope.pages[page];
                 $scope.currentPage = page;
+                $scope.isInTransit = true;
             }
         };
 
         $scope.isCurrentPage = function (page) {
             return $scope.currentPage === page;
         };
+
+        $scope.$on('bgTransitionComplete', function(){
+            $scope.isInTransit = false;
+        });
     })
     .directive('bg', function ($window) {
         // Adapted from http://bavotasan.com/2011/full-sizebackground-image-jquery-plugin/ Thanks @bavotasan!
@@ -58,6 +64,43 @@ angular.module('website', ['ngAnimate'])
         return {
             restrict: 'A',
             link: linker
+        };
+    })
+    .animation('.bg-animation', function ($window, $rootScope) {
+        return {
+            enter: function (element, done) {
+                TweenMax.fromTo(element, 0.5, { left: $window.innerWidth}, {left: 0, onComplete: function () {
+                    $rootScope.$apply(function(){
+                        $rootScope.$broadcast('bgTransitionComplete');
+                    });
+                    done();
+                }});
+            },
+
+            leave: function (element, done) {
+                TweenMax.to(element, 0.5, {left: -$window.innerWidth, onComplete: done});
+            }
+        };
+    })
+    .animation('.panel-animation', function () {
+        return {
+            addClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    TweenMax.to(element, 0.2, { alpha: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+                    TweenMax.fromTo(element, 0.5, { alpha: 0, left: -element.width() }, { alpha: 0.8, left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
         };
     });
 
