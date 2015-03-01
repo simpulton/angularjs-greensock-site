d7b250e9d080ce7722e3bf9b33419a245d0eb5df
Lukas Ruebbelke <simpul@gmail.com>
2015-02-28 23:35:18 -0700

    Updating to latest

--- js/app.finish.js
+++ js/app.finish.js
@@ -1,107 +1,106 @@
 angular.module('website', ['ngAnimate'])
-    .constant('TweenMax', TweenMax)
-    .controller('MainCtrl', function ($scope) {
-        $scope.pages = {
-            'home': { label: 'Home', sublabel: 'Sublabel', content: 'This is page content.' },
-            'about': { label: 'About', sublabel: 'Sublabel', content: 'This is page content.' },
-            'contact': { label: 'Contact', sublabel: 'Sublabel', content: 'This is page content.' }
-        };
+  .constant('TweenMax', TweenMax)
+  .controller('MainCtrl', function ($scope) {
+    $scope.pages = {
+      'home': {label: 'Home', sublabel: 'Sublabel', content: 'This is page content.'},
+      'about': {label: 'About', sublabel: 'Sublabel', content: 'This is page content.'},
+      'contact': {label: 'Contact', sublabel: 'Sublabel', content: 'This is page content.'}
+    };
 
-        $scope.currentPage = 'home';
-        $scope.page = $scope.pages['home'];
-        $scope.isInTransit = false;
+    $scope.currentPage = 'home';
+    $scope.page = $scope.pages['home'];
+    $scope.isInTransit = false;
 
-        $scope.setCurrentPage = function (page) {
-            if ($scope.currentPage !== page) {
-                $scope.page = $scope.pages[page];
-                $scope.currentPage = page;
-                $scope.isInTransit = true;
-            }
-        };
+    $scope.setCurrentPage = function (page) {
+      if ($scope.currentPage !== page) {
+        $scope.page = $scope.pages[page];
+        $scope.currentPage = page;
+        $scope.isInTransit = true;
+      }
+    };
 
-        $scope.isCurrentPage = function (page) {
-            return $scope.currentPage === page;
-        };
+    $scope.isCurrentPage = function (page) {
+      return $scope.currentPage === page;
+    };
 
-        $scope.$on('bgTransitionComplete', function(){
-            $scope.isInTransit = false;
-        });
-    })
-    .directive('bg', function ($window) {
-        // Adapted from http://bavotasan.com/2011/full-sizebackground-image-jquery-plugin/ Thanks @bavotasan!
-        var linker = function (scope, element, attrs) {
-            var resizeBG = function () {
-                var bgwidth = element.width();
-                var bgheight = element.height();
-
-                var winwidth = $window.innerWidth;
-                var winheight = $window.innerHeight;
-
-                var widthratio = winwidth / bgwidth;
-                var heightratio = winheight / bgheight;
-
-                var widthdiff = heightratio * bgwidth;
-                var heightdiff = widthratio * bgheight;
-
-                if (heightdiff > winheight) {
-                    element.css({
-                        width: winwidth + 'px',
-                        height: heightdiff + 'px'
-                    });
-                } else {
-                    element.css({
-                        width: widthdiff + 'px',
-                        height: winheight + 'px'
-                    });
-                }
-            }
-
-            resizeBG();
-
-            var windowElement = angular.element($window);
-            windowElement.resize(resizeBG);
-        }
-
-        return {
-            restrict: 'A',
-            link: linker
-        };
-    })
-    .animation('.bg-animation', function ($window, $rootScope, TweenMax) {
-        return {
-            enter: function (element, done) {
-                TweenMax.fromTo(element, 0.5, { left: $window.innerWidth}, {left: 0, onComplete: function () {
-                    $rootScope.$apply(function(){
-                        $rootScope.$broadcast('bgTransitionComplete');
-                    });
-                    done();
-                }});
-            },
-
-            leave: function (element, done) {
-                TweenMax.to(element, 0.5, {left: -$window.innerWidth, onComplete: done});
-            }
-        };
-    })
-    .animation('.panel-animation', function (TweenMax) {
-        return {
-            addClass: function (element, className, done) {
-                if (className == 'ng-hide') {
-                    TweenMax.to(element, 0.2, { opacity: 0, onComplete: done });
-                }
-                else {
-                    done();
-                }
-            },
-            removeClass: function (element, className, done) {
-                if (className == 'ng-hide') {
-                    element.removeClass('ng-hide');
-                    TweenMax.fromTo(element, 0.5, { opacity: 0, left: -element.width() }, { opacity: 0.8, left: 0, onComplete: done });
-                }
-                else {
-                    done();
-                }
-            }
-        };
+    $scope.$on('bgTransitionComplete', function () {
+      $scope.isInTransit = false;
     });
+  })
+  .directive('bg', function ($window) {
+    return function (scope, element, attrs) {
+      var resizeBG = function () {
+        var bgwidth = element.width();
+        var bgheight = element.height();
+
+        var winwidth = $window.innerWidth;
+        var winheight = $window.innerHeight;
+
+        var widthratio = winwidth / bgwidth;
+        var heightratio = winheight / bgheight;
+
+        var widthdiff = heightratio * bgwidth;
+        var heightdiff = widthratio * bgheight;
+
+        if (heightdiff > winheight) {
+          element.css({
+            width: winwidth + 'px',
+            height: heightdiff + 'px'
+          });
+        } else {
+          element.css({
+            width: widthdiff + 'px',
+            height: winheight + 'px'
+          });
+        }
+      };
+
+      var windowElement = angular.element($window);
+      windowElement.resize(resizeBG);
+
+      element.bind('load', function () {
+        resizeBG();
+      });
+    }
+  })
+  .animation('.bg-animation', function ($window, $rootScope, TweenMax) {
+    return {
+      enter: function (element, done) {
+        TweenMax.fromTo(element, 0.5, {left: $window.innerWidth}, {
+          left: 0, onComplete: function () {
+            $rootScope.$apply(function () {
+              $rootScope.$broadcast('bgTransitionComplete');
+            });
+            done();
+          }
+        });
+      },
+
+      leave: function (element, done) {
+        TweenMax.to(element, 0.5, {left: -$window.innerWidth, onComplete: done});
+      }
+    };
+  })
+  .animation('.panel-animation', function (TweenMax) {
+    return {
+      addClass: function (element, className, done) {
+        if (className == 'ng-hide') {
+          TweenMax.to(element, 0.2, {opacity: 0, onComplete: done});
+        }
+        else {
+          done();
+        }
+      },
+      removeClass: function (element, className, done) {
+        if (className == 'ng-hide') {
+          element.removeClass('ng-hide');
+          TweenMax.fromTo(element, 0.5, {opacity: 0, left: -element.width()}, {opacity: 0.8, left: 0, onComplete: done});
+        }
+        else {
+          done();
+        }
+      }
+    };
+  })
+;
 
--- js/app.js
+++ js/app.js
@@ -1,63 +1,60 @@
 angular.module('website', [])
-    .controller('MainCtrl', function ($scope) {
-        $scope.pages = {
-            'home': { label: 'Home', sublabel: 'Sublabel', content: 'This is page content.' },
-            'about': { label: 'About', sublabel: 'Sublabel', content: 'This is page content.' },
-            'contact': { label: 'Contact', sublabel: 'Sublabel', content: 'This is page content.' }
-        };
+  .controller('MainCtrl', function ($scope) {
+    $scope.pages = {
+      'home': {label: 'Home', sublabel: 'Sublabel', content: 'This is page content.'},
+      'about': {label: 'About', sublabel: 'Sublabel', content: 'This is page content.'},
+      'contact': {label: 'Contact', sublabel: 'Sublabel', content: 'This is page content.'}
+    };
 
-        $scope.currentPage = 'home';
-        $scope.page = $scope.pages['home'];
+    $scope.currentPage = 'home';
+    $scope.page = $scope.pages['home'];
 
-        $scope.setCurrentPage = function (page) {
-            if ($scope.currentPage !== page) {
-                $scope.page = $scope.pages[page];
-                $scope.currentPage = page;
-            }
-        };
+    $scope.setCurrentPage = function (page) {
+      if ($scope.currentPage !== page) {
+        $scope.page = $scope.pages[page];
+        $scope.currentPage = page;
+      }
+    };
 
-        $scope.isCurrentPage = function (page) {
-            return $scope.currentPage === page;
-        };
-    })
-    .directive('bg', function ($window) {
-        // Adapted from http://bavotasan.com/2011/full-sizebackground-image-jquery-plugin/ Thanks @bavotasan!
-        var linker = function (scope, element, attrs) {
-            var resizeBG = function () {
-                var bgwidth = element.width();
-                var bgheight = element.height();
+    $scope.isCurrentPage = function (page) {
+      return $scope.currentPage === page;
+    };
+  })
+  .directive('bg', function ($window) {
+    return function (scope, element, attrs) {
+      var resizeBG = function () {
+        var bgwidth = element.width();
+        var bgheight = element.height();
 
-                var winwidth = $window.innerWidth;
-                var winheight = $window.innerHeight;
+        var winwidth = $window.innerWidth;
+        var winheight = $window.innerHeight;
 
-                var widthratio = winwidth / bgwidth;
-                var heightratio = winheight / bgheight;
+        var widthratio = winwidth / bgwidth;
+        var heightratio = winheight / bgheight;
 
-                var widthdiff = heightratio * bgwidth;
-                var heightdiff = widthratio * bgheight;
+        var widthdiff = heightratio * bgwidth;
+        var heightdiff = widthratio * bgheight;
 
-                if (heightdiff > winheight) {
-                    element.css({
-                        width: winwidth + 'px',
-                        height: heightdiff + 'px'
-                    });
-                } else {
-                    element.css({
-                        width: widthdiff + 'px',
-                        height: winheight + 'px'
-                    });
-                }
-            }
-
-            resizeBG();
-
-            var windowElement = angular.element($window);
-            windowElement.resize(resizeBG);
+        if (heightdiff > winheight) {
+          element.css({
+            width: winwidth + 'px',
+            height: heightdiff + 'px'
+          });
+        } else {
+          element.css({
+            width: widthdiff + 'px',
+            height: winheight + 'px'
+          });
         }
+      };
 
-        return {
-            restrict: 'A',
-            link: linker
-        };
-    });
+      var windowElement = angular.element($window);
+      windowElement.resize(resizeBG);
+
+      element.bind('load', function () {
+        resizeBG();
+      });
+    }
+  })
+;
 
